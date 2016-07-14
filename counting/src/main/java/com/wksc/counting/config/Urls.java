cb80d7524708d45ad31a8faba555c82ecad6b3b2
puhuao <puhuao@sina.com>
2016-07-15 00:07:59 +0800

    修改ip地址

--- counting/src/main/java/com/wksc/counting/config/Urls.java
+++ counting/src/main/java/com/wksc/counting/config/Urls.java
@@ -4,9 +4,9 @@ package com.wksc.counting.config;
  * Created by Administrator on 2016/7/2.
  */
 public class Urls {
-    public static final String BASE_URL = "http://10.1.100.6:8087/ea/gw?cmd=";
+    public static final String BASE_URL = "http://mbasys.1919.cn:8880/gw?cmd=";
 //    public static final String BASE_URL = "http://bpb.1919.cn/ea/gw?cmd=";
-//    public static final String BASE_URL = "http://192.168.8.131:8080/gw?cmd=";
+//    public static final String BASE_URL = "http://118.123.209.13:8880/ea/gw?cmd=";
     public static final String BASE_INFO = BASE_URL+"appGetBaseInfo";
     public static final String LOGIN = BASE_URL+"memberLogin";
     public static final String COREINDEX = BASE_URL+"appCoreIndex";
